b58ca85deb523808e2d406fb2329f2a06e73ca1d
Davut Çağrı Akbıyık <davutcagria@gmail.com>
2024-11-16 18:16:37 +0400

    Second Commit

--- kafka-consumer/src/main/java/davutcagri/kafka_consumer/ConsumerConfig.java
+++
@@ -1,9 +0,0 @@
-package davutcagri.kafka_consumer;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
-
-@Configuration
-@EnableKafka
-public class ConsumerConfig {
-}
